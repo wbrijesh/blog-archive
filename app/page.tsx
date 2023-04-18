705478f04f6378058f26d881f4821ad6945e1691
Brijesh Wawdhane <brijesh@wawdhane.com>
2023-04-19 03:14:54 +0530

    fix: unescaped ampersand build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <>
       <div className="m-3 flex flex-col gap-4">
-        <h1 className="text-lg font-bold">Brijesh's Blog (coming soon)</h1>
+        <h1 className="text-lg font-bold">Brijesh{"'"}s Blog (coming soon)</h1>
         <p>Hi, I am Brijesh, student and web developer</p>
         <div>
           <p>I am planning on writing blog posts considtently on this site, and am currently buildig this site</p>
